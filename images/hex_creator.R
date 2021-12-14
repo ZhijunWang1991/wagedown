0545c3511be6f818beba4f8963485fdb01e50e72
ZhijunWang1991 <zhijun.wang@wur.nl>
2021-12-14 15:06:02 +0100

    up

--- images/hex_creator.R
+++ images/hex_creator.R
@@ -1,16 +1,16 @@
 library(hexSticker)
 
 # https://pixabay.com/illustrations/graduation-hats-throw-graduation-4906359/
-cap <- "images/happy_cap.png"
+img <- "images/guinea_pig.png"
 
-sticker(cap,
-  package = "thesisdown",
-  p_family = "Aller_Rg",
-  p_size = 7, p_color = "#5B5040", # p_x = 4, p_y = 1.4,
-  s_x = 1, s_y = .8, s_width = 0.75, s_height = 0.75,
-  h_fill = "#FAECC7", h_color = "#5B5040",
-  filename = "thesisdown_hex.png",
-  url = "thesisdown.rbind.io"
+sticker(img,
+        package = "wagedown",
+        p_family = "Aller_Rg",
+        p_size = 25, p_color = "#5B5040", # p_x = 4, p_y = 1.4,
+        s_x = 1, s_y = 0.8, s_width = 0.3, s_height = 0.3,
+        h_fill = "#FAECC7", h_color = "#5B5040",
+        filename = "wagedown_hex.png",
+        url = "wagedown.rbind.io"
 )
 
 # Final image was revised in Adobe Illustrator
